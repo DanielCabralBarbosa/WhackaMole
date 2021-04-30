d2eb5d62a7a4dbfd5ee4ac415db146cf4060f3a9
DanielCabralBarbosa <77117506+DanielCabralBarbosa@users.noreply.github.com>
2021-04-30 01:30:17 -0300

    Update MultiTouchActivity.java

--- app/src/main/java/com/shithappens/whack_a_mole/MultiTouchActivity.java
+++ app/src/main/java/com/shithappens/whack_a_mole/MultiTouchActivity.java
@@ -22,11 +22,36 @@ public class MultiTouchActivity extends AppCompatActivity {
             super(context);
             points = new Point[MAX_TOUCHES];
             isVisible = new boolean[MAX_TOUCHES];
+
         }
         @Override
         public boolean onTouch(View v, MotionEvent event) {
+
+            int action = event.getActionMasked();
+            int pointerIndex = event.getActionIndex();
+            int id = event.getPointerId(pointerIndex);
+
+            switch (action){
+                case MotionEvent.ACTION_DOWN:
+                case MotionEvent.ACTION_POINTER_DOWN:
+
+                    points[id] = new Point();
+                    points[id].x = (int) event.getX(pointerIndex);
+                    points[id].y = (int) event.getY(pointerIndex);
+                    isVisible[id] = true;
+                    break;
+
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_POINTER_UP:
+                isVisible[id] = false;
+            }
+
             return false;
         }
+
+
+
+
     }
 
     @Override
